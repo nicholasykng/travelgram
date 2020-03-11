dcf6f235dede73dcca658d350cdc99f074e85197
nicholasykng <nicholasykng@gmail.com>
2020-03-10 19:49:24 -0700

    parsing out the location form fields

--- travelgram-frontend/index.js
+++ travelgram-frontend/index.js
@@ -1,9 +1,10 @@
-locationBox = document.querySelector('section#location-box')
+document.addEventListener("DOMContentLoaded", () => {
+    locationBox = document.querySelector('section#location-box')
+})
 function getLocations() {
     fetch('http://localhost:3000/locations')
     .then(response => response.json())
     .then(locations => locations.forEach((location) => {
-        locationBox = document.querySelector('section#location-box')
         locationBox.innerHTML = `
         <div data-id=${location.id}>
         <div class="location-pics">
@@ -21,4 +22,46 @@ function getLocations() {
         })
     }) 
     )}
-getLocations()
+getLocations();
+
+const locationFormFields = `
+    <label>Picture URL</label><br>
+    <input type="text" id="picture"><br>
+    <label>Description</label><br>
+    <input type="textarea" id="description"><br>
+    <input type="submit" value="Add New Location">
+    </form>
+    `
+class Location {
+    constructor(data) {
+        this.picture = data.picture
+        this.description = data.description
+    }
+    static newLocationForm() {
+         let addLocation = document.querySelector('section#add_location')
+         addLocation.innerHTML = `
+         <form onsubmit="createLocation(); return false;">` + locationFormFields
+    } 
+}
+
+function createLocation() {
+    const location = {
+        picture: document.getElementById('picture').value,
+        description: document.getElementById("description").value
+    }
+    fetch("http://localhost:3000/locations", {
+        method: "POST",
+        body: JSON.stringify(location),
+        headers: {'Content-Type': 'application/json', "Accept": "application/json"}
+    })
+    .then(response => response.json())
+    .then(location => {
+        Location.newLocationForm()
+        clearLocationHtml()
+        getLocations()
+    })
+}
+
+function clearLocationHtml() {
+    locationBox.innerHTML = ""
+}
